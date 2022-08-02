9d57eafcafd23924c047f24a57b17ed839689af4
gusscamargo <guga.power@hotmail.com>
2022-08-02 17:17:49 -0300

    Adicionando main file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,51 @@
+#[allow(unused_macros)]
+macro_rules! read {
+    ($out:ident as $type:ty) => {
+        let mut inner = String::new();
+        std::io::stdin().read_line(&mut inner).expect("A String");
+        let $out = inner.trim().parse::<$type>().expect("Parsable");
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! read_str {
+    ($out:ident) => {
+        let mut inner = String::new();
+        std::io::stdin().read_line(&mut inner).expect("A String");
+        let $out = inner.trim();
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! read_vec {
+    ($out:ident as $type:ty) => {
+        let mut inner = String::new();
+        std::io::stdin().read_line(&mut inner).unwrap();
+        let $out = inner
+            .trim()
+            .split_whitespace()
+            .map(|s| s.parse::<$type>().unwrap())
+            .collect::<Vec<$type>>();
+    };
+}
+
 fn main() {
     println!("Hello, world!");
 }
+
+// fn problema(n: i64) -> i64 {
+	
+// }
+
+// #[cfg(test)]
+// mod test {
+// 	use super::*;
+
+// 	#[test]
+// 	fn test_1() {
+// 		let p = problema(8);
+// 		let r = 50;
+
+// 		assert_eq!(p, r);
+// 	}
+// } 
